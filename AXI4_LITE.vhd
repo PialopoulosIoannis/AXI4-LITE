5f85d1ace753daef84fc085fa3b4ae8c06f9f0dc
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-07 17:42:58 +0300

    added Write Data Channel

--- AXI4_LITE.vhd
+++ AXI4_LITE.vhd
@@ -6,8 +6,8 @@ PORT ( ACLK, ARESETN : in STD_LOGIC;  -- clock and reset
        ARADDR, ARCACHE, ARPROT, ARVALID, ARREADY : in STD_LOGIC; -- Read Address Channel
        RDATA, RRESP, RVALID, RREADY : out STD_LOGIC; -- Read Data Channel
        AWADDR, AWCACHE, AWPROT, AWVALID : in STD_LOGIC; -- Line 8,9 are Write Address Channel
-       AWREADY : out STD_LOGIC;
-       
+       AWREADY : out STD_LOGIC; 
+       WDATA, WSTRB : in STD_LOGIC; -- Write Data Channel
 
 
 )
