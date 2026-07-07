583b12fb88b2c971e6cd5da1fc419c67896e7875
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-07 18:34:12 +0300

    Corrected tje read address channel ports

--- AXI4_LITE.vhd
+++ AXI4_LITE.vhd
@@ -3,7 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity AXI4_LITE_RAM is 
 PORT ( ACLK, ARESETN : in STD_LOGIC;  -- clock and reset
-       ARADDR, ARCACHE, ARPROT, ARVALID, ARREADY : in STD_LOGIC; -- Read Address Channel
+       ARADDR, ARCACHE, ARPROT, ARVALID : in STD_LOGIC; -- Line 6,7 Read Address Channel
+       ARREADY : out STD_LOGIC;
        RDATA, RRESP, RVALID, RREADY : out STD_LOGIC; -- Read Data Channel
        AWADDR, AWCACHE, AWPROT, AWVALID : in STD_LOGIC; -- Line 8,9 are Write Address Channel
        AWREADY : out STD_LOGIC; 
