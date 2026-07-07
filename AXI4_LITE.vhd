83b583b1e7152236d63119549b29a630e15f81b4
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-07 17:35:10 +0300

    added Read Data Channel to entity

--- AXI4_LITE.vhd
+++ AXI4_LITE.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity AXI4_LITE_RAM is 
 PORT ( ACLK, ARESETN : in STD_LOGIC;  -- clock and reset
        ARADDR, ARCACHE, ARPROT, ARVALID, ARREADY : in STD_LOGIC; -- Read Address Channel
-
+       RDATA, RRESP, RVALID, RREADY : out STD_LOGIC; -- Read Data Channel
 
 
 
