887af7f56f31bf7d810ed1a7d1d0cc60f60c7a55
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-07 17:30:09 +0300

    Added the Read Adress Channel to entity

--- AXI4_LITE.vhd
+++ AXI4_LITE.vhd
@@ -2,7 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity AXI4_LITE_RAM is 
-PORT ( ACLK, ARESETN : in STD_LOGIC;
+PORT ( ACLK, ARESETN : in STD_LOGIC;  -- clock and reset
+       ARADDR, ARCACHE, ARPROT, ARVALID, ARREADY : in STD_LOGIC; -- Read Address Channel
+
 
 
 
