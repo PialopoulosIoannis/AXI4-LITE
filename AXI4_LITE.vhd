84f33b4ba2b90345def08657861be8af41407058
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-07 17:41:42 +0300

    added Write Address Channel

--- AXI4_LITE.vhd
+++ AXI4_LITE.vhd
@@ -5,8 +5,9 @@ entity AXI4_LITE_RAM is
 PORT ( ACLK, ARESETN : in STD_LOGIC;  -- clock and reset
        ARADDR, ARCACHE, ARPROT, ARVALID, ARREADY : in STD_LOGIC; -- Read Address Channel
        RDATA, RRESP, RVALID, RREADY : out STD_LOGIC; -- Read Data Channel
-
-
+       AWADDR, AWCACHE, AWPROT, AWVALID : in STD_LOGIC; -- Line 8,9 are Write Address Channel
+       AWREADY : out STD_LOGIC;
+       
 
 
 )
